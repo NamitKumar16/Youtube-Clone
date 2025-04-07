74b8f672e26c225dd8da72774a4221aa706efe68
NamitKumar16 <kumarnamit58@gmail.com>
2025-04-08 02:46:54 +0530

    Update middleware to protect /studio route; add StudioUploadModal component and enhance StudioSidebarHeader with user profile details

--- src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
+++ src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
@@ -1,11 +1,44 @@
-import { SidebarHeader } from "@/components/ui/sidebar";
+import {
+  SidebarHeader,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { UserAvatar } from "@/components/user-avatar";
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 
 const StudioSidebarHeader = () => {
   const { user } = useUser();
-  if (!user) return null;
+  const { state } = useSidebar();
+  if (!user) {
+    return (
+      <SidebarHeader className="flex items-center justify-center pb-4">
+        <Skeleton className="size-[112px] rounded-full" />
+        <div className="flex flex-col items-center mt-2 gap-y-2">
+          <Skeleton className="h-4 w-[80px]" />
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      </SidebarHeader>
+    );
+  }
+  if (state === "collapsed") {
+    return (
+      <SidebarMenuItem>
+        <SidebarMenuButton tooltip="Your profile" asChild>
+          <Link href="/users/current">
+            <UserAvatar
+              imageUrl={user?.imageUrl}
+              name={user?.fullName ?? "User"}
+              size={"xs"}
+            />
+            <span className="text-sm">Your Profile</span>
+          </Link>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    );
+  }
 
   return (
     <SidebarHeader className="flex items-center justify-center pb-4">
@@ -16,6 +49,12 @@ const StudioSidebarHeader = () => {
           className="size-[112px] hover:opacity-80 transition-opacity"
         />
       </Link>
+      <div className="flex flex-col items-center mt-2 gap-y-1">
+        <p className="text-sm font-medium">Your Profile</p>
+        <p className="text-xs text-muted-foreground">
+          {user?.fullName ?? "User"}
+        </p>
+      </div>
     </SidebarHeader>
   );
 };
