1ae2c356a601e8c294b87b9ad60e191b6cd065f1
NamitKumar16 <kumarnamit58@gmail.com>
2025-04-09 01:16:13 +0530

    Implement Studio page with TRPC integration and StudioView component

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,5 +1,13 @@
-const Page = () => {
-  return <div>Studio</div>;
+import StudioView from "@/modules/studio/ui/view/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+const Page = async () => {
+  void trpc.studio.getMany.prefetchInfinite();
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 };
 
 export default Page;
---
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -0,0 +1,5 @@
+const StudioView = () => {
+  return <div>Hello</div>;
+};
+
+export default StudioView;
