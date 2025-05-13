0e8fcc2d53547b8d5788002072c5b2911a1abeed
NamitKumar16 <kumarnamit58@gmail.com>
2025-05-14 01:29:54 +0530

    feat: implement video detail page with video view and form section, enhance videos section with skeleton loading

--- src/modules/studio/server/procedures.tsx
+++ src/modules/studio/server/procedures.tsx
@@ -1,10 +1,27 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import { and, desc, eq, lt, or } from "drizzle-orm";
 import { z } from "zod";
 
 export const studioRouter = createTRPCRouter({
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .query(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+      const { id } = input;
+
+      const [video] = await db
+        .select()
+        .from(videos)
+        .where(and(eq(videos.userId, userId), eq(videos.id, id)));
+      if (!video) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Video not found" });
+      }
+      return video;
+    }),
+
   getMany: protectedProcedure
     .input(
       z.object({
