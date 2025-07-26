736a87bceceecab465eadedc7ec4d88b998b86eb
NamitKumar16 <kumarnamit58@gmail.com>
2025-07-26 21:37:33 +0530

    feat: implement comments section with form and error handling

---
+++ src/modules/comments/ui/components/comment-form.tsx
@@ -0,0 +1,98 @@
+import { z } from "zod";
+import { toast } from "sonner";
+import { trpc } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import { commentInsertSchema } from "@/db/schema";
+import { useUser, useClerk } from "@clerk/nextjs";
+import { Textarea } from "@/components/ui/textarea";
+import { UserAvatar } from "@/components/user-avatar";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+interface CommentFormProps {
+  videoId: string;
+  onSuccess?: () => void;
+}
+
+export const CommentForm = ({ videoId, onSuccess }: CommentFormProps) => {
+  const { user } = useUser();
+  const clerk = useClerk();
+
+  const utils = trpc.useUtils();
+
+  const create = trpc.comments.create.useMutation({
+    onSuccess: () => {
+      utils.comments.getMany.invalidate({ videoId });
+      form.reset();
+      toast.success("Comment Added");
+      onSuccess?.();
+    },
+
+    onError: (error) => {
+      toast.error("Something went wrong");
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const formSchema = commentInsertSchema.omit({ userId: true });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      videoId,
+      value: "",
+    },
+  });
+
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    create.mutate(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className="flex gap-4 group"
+        onSubmit={form.handleSubmit(handleSubmit)}
+      >
+        <UserAvatar
+          size="lg"
+          imageUrl={user?.imageUrl || "/user-placeholder.svg"}
+          name={user?.username || "User"}
+        />
+        <div className="flex-1">
+          <FormField
+            name="value"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="Add a Comment..."
+                    className="resize-none bg-transparent overflow-hidden min-h-0"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="justify-end gap-2 mt-2 flex">
+            <Button type="submit" size="sm" disabled={create.isPending}>
+              Comment
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Form>
+  );
+};
--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
 
-const CommentsSection = () => {
-  return <div>CommentsSection</div>;
+import { CommentForm } from "@/modules/comments/ui/components/comment-form";
+import { trpc } from "@/trpc/client";
+import React, { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+interface CommentsSectionProps {
+  videoId: string;
+}
+
+const CommentsSection = ({ videoId }: CommentsSectionProps) => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <CommentsSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
+  const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId });
+
+  return (
+    <div className="mt-6 ">
+      <div className="flex flex-col gap-6">
+        <h1>0 Comments</h1>
+        <CommentForm videoId={videoId} onSuccess={() => {}} />
+      </div>
+      {JSON.stringify(comments)}
+    </div>
+  );
 };
 
 export default CommentsSection;
