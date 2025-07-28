a77fe6e642e0d41d09ccfc7ee8eacac8847a9fcb
NamitKumar16 <kumarnamit58@gmail.com>
2025-07-29 01:35:11 +0530

    feat: enhance comments functionality with user details and display components

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CommentForm } from "@/modules/comments/ui/components/comment-form";
+import { CommentItem } from "@/modules/comments/ui/components/comment-item";
 import { trpc } from "@/trpc/client";
 import React, { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -25,10 +26,14 @@ const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
   return (
     <div className="mt-6 ">
       <div className="flex flex-col gap-6">
-        <h1>0 Comments</h1>
+        <h1>{comments.length} Comments</h1>
         <CommentForm videoId={videoId} onSuccess={() => {}} />
+        <div className="flex flex-col gap-4 mt-2">
+          {comments.map((comment) => (
+            <CommentItem key={comment.id} comment={comment} />
+          ))}
+        </div>
       </div>
-      {JSON.stringify(comments)}
     </div>
   );
 };
