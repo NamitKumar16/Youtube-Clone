abaacfa95caa4d8d83382e55684ca3234ce49879
NamitKumar16 <kumarnamit58@gmail.com>
2025-09-09 21:35:40 +0530

    feat: implement banner upload functionality with modal for user profile, allowing users to upload and manage their banner images

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -4,6 +4,8 @@ import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Edit2Icon } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { BannerUploadModal } from "./banner-upload-modal";
+import { useState } from "react";
 
 interface UserPageBannerProps {
   user: UserGetOneOutput;
@@ -15,9 +17,15 @@ export const UserPageBannerSkeleton = () => {
 
 const UserPageBanner = ({ user }: UserPageBannerProps) => {
   const { userId } = useAuth();
+  const [isBannerUploadModalOpen, setIsBannerUploadModalOpen] = useState(false);
   const { bannerUrl, clerkId } = user;
   return (
     <div className="relative group">
+      <BannerUploadModal
+        userId={user.id}
+        open={isBannerUploadModalOpen}
+        onOpenChange={setIsBannerUploadModalOpen}
+      />
       <div
         className={cn(
           "w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl",
@@ -27,6 +35,7 @@ const UserPageBanner = ({ user }: UserPageBannerProps) => {
       >
         {clerkId === userId ? (
           <Button
+            onClick={() => setIsBannerUploadModalOpen(true)}
             type="button"
             size="icon"
             className="absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black/70 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
